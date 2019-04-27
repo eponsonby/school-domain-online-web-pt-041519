9e4445223082831a295c4940320dfd72b4a486c5
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-27 15:24:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ attr_accessor :name, :roster
   end
   
   def add_student(name, grade)
-    @roster[grade] ||= []
+    #@roster[grade] ||= []
+    @roster[grade] = roster[grade] || []
     @roster[grade] << name
   end
   
